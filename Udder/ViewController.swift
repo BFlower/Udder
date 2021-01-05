30d5b10af1c2525795138d76225ec413ef11901f
Bekemgul <r.bekemgul@gmail.com>
2021-01-06 00:55:06 +0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Bekemgul Rizuanova on 2021-01-06.
+//  Copyright © 2021 Bekemgul Rizuanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
